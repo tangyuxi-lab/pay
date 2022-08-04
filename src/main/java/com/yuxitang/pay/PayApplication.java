99f117a1e897d68241ce7737fefc1ce70439debe
Yuxi Tang <1311076274@qq.com>
2022-08-04 16:00:20 -0400

    Finished Pay server

--- src/main/java/com/yuxitang/pay/PayApplication.java
+++ src/main/java/com/yuxitang/pay/PayApplication.java
@@ -1,9 +1,11 @@
 package com.yuxitang.pay;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.yuxitang.pay.dao")
 public class PayApplication {
 
     public static void main(String[] args) {
